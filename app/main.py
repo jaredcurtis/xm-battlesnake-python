0fe3f28d265c481aabe213baaa861a141a68ad69
jrubert@xmatters.com <jrubert@xmatters.com>
2019-07-11 12:33:15 -0700

    pacifist snake

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import bottle
 import os
 import random
+import json
+import sys
 
 
 @bottle.route('/static/<path:path>')
@@ -14,6 +16,7 @@ def start():
     game_id = data['game_id']
     board_width = data['width']
     board_height = data['height']
+
 
     head_url = '%s://%s/static/head.png' % (
         bottle.request.urlparts.scheme,
@@ -34,7 +37,7 @@ def start():
 def move():
     data = bottle.request.json
     return {
-        'move': findFood(data),
+        'move': findFood(data, None),
         'taunt': 'Come get some!'
     }
 
@@ -42,25 +45,80 @@ def getSnake(gameState, id):
    for snake in gameState["snakes"]:
        if snake["id"] == id :
         return snake
-def findFood(gameState):
-
+def findFood(gameState, invalidMoves):
+  if invalidMoves is None:
+        invalidMoves = []
   mySnake = getSnake(gameState, gameState["you"])
   head = mySnake["coords"][0]
-  if gameState["food"][0][0] < head[0]: 
+  move = None
+  if gameState["food"][0][0] < head[0] and isValidMove(gameState, "left", head):
         move = "left"
+        invalidMoves.append("left")
 
-
-  if gameState["food"][0][0] > head[0]:
+  if gameState["food"][0][0] > head[0] and isValidMove(gameState, "right", head):
         move = "right"
+        invalidMoves.append("right")
 
-
-  if gameState["food"][0][1] < head[1]:
+  if gameState["food"][0][1] < head[1] and isValidMove(gameState, "up", head):
         move = "up"
+        invalidMoves.append("up")
 
-
-  if gameState["food"][0][1] > head[1]:
+  if gameState["food"][0][1] > head[1] and isValidMove(gameState, "down", head):
         move = "down"
-  return move 
+        invalidMoves.append("down")
+
+  if move is None:
+      move = pickNextMove(gameState, invalidMoves, head)
+
+  print ("chosen move ", move)
+  sys.stdout.flush()
+  return move
+
+def pickNextMove(gameState, invalidMoves, head):
+  if "left" not in invalidMoves:
+      if isValidMove(gameState, "left", head):
+          return "left"
+  if "right" not in invalidMoves:
+      if isValidMove(gameState, "right", head):
+          return "right"
+  if "up" not in invalidMoves:
+      if isValidMove(gameState, "up", head):
+          return "up"
+  return "down"
+
+def transformMove(move, head):
+  if move == "left":
+      return [head[0] - 1, head[1]]
+  if move == "right":
+      return [head[0] + 1, head[1]]
+  if move == "up":
+      return [head[0], head[1] - 1]
+  if move == "down":
+      return [head[0], head[1] + 1]
+
+def isValidMove(gameState, move, head):
+  return isNotCollidingSnake(gameState, move, head) and isNotCollidingWall(gameState, move, head)
+
+def isNotCollidingSnake(gameState, move, head):
+  realMove = transformMove(move, head)
+  isValid = True
+  for snake in gameState["snakes"]:
+        for coord in snake["coords"]:
+                if coord[0] == realMove[0] and coord[1] == realMove[1]:
+                    isValid = False
+  print ("checking snake collision ",move, realMove[0], realMove[1], isValid)
+  sys.stdout.flush()
+  return isValid
+
+def isNotCollidingWall(gameState, move, head):
+  realMove = transformMove(move, head)
+  isValid = True
+  if realMove[0] >= gameState["height"] or realMove[0] < 0:
+      isValid = False
+  if realMove[1] >= gameState["width"] or realMove[1] < 0:
+      isValid = False
+  print ("checking walls ", realMove[0], realMove[1], isValid)
+  return isValid
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
